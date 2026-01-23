9fe61b4263b9f94e17d370a646d8811f6a7855e8
Simby1 <kimsimby@gmail.com>
2026-01-23 23:54:59 +0100

    chore: add Sentry for error tracking in the application

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode, ErrorInfo } from 'react';
+import * as Sentry from '@sentry/react';
 
 interface State {
   hasError: boolean;
@@ -14,19 +15,18 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
     this.state = { hasError: false };
   }
 
-  // This updates the state so the next render shows the fallback UI
   static getDerivedStateFromError(error: Error): State {
     return { hasError: true };
   }
 
-  // This is where you log the error (we'll add Sentry here later!)
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     console.log({ error, errorInfo });
+
+    Sentry.captureException(error, { extra: { errorInfo } });
   }
 
   render() {
     if (this.state.hasError) {
-      // This is the Fallback UI the user sees when things break
       return (
         <div style={{ padding: "20px", textAlign: "center" }}>
           <h2>Oops, there is an error!</h2>
